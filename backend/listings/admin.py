13f59becf566497afd17a0d546b94eaef16bcd01
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-06 23:12:19 +0600

    update admin and urls in listings app

--- backend/listings/admin.py
+++ backend/listings/admin.py
@@ -5,7 +5,7 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
     list_display_links = ('id', 'title')
     list_filter = ('realtor', )
-    list_editable = ('is_published', )
+    list_editable = ('is_published','price','realtor' )
     search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
     list_per_page = 25
 
