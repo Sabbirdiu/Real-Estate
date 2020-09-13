119f79227991a1c39abd7425922b35123f22148b
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-13 15:09:34 +0600

    added design to auth

--- backend/realstate/settings.py
+++ backend/realstate/settings.py
@@ -144,13 +144,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
-    
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3
     
 }
