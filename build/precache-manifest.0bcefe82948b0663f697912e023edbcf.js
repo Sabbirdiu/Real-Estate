8ad73f9b2d958eb5bc77c561529d8c5a370db9b6
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-17 22:16:53 +0600

    rewrite the file

---
+++ build/precache-manifest.0bcefe82948b0663f697912e023edbcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a67c1bfe1977d0fd6c83a7c2e7c82b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be72afcb28bee0e1bb5",
+    "url": "/static/css/main.502f2429.chunk.css"
+  },
+  {
+    "revision": "6608455b2a890664e27a",
+    "url": "/static/js/2.83d42219.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83d42219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be72afcb28bee0e1bb5",
+    "url": "/static/js/main.c48e5523.chunk.js"
+  },
+  {
+    "revision": "c1fa34341f4c25b8e620",
+    "url": "/static/js/runtime-main.a5837ae2.js"
+  },
+  {
+    "revision": "54ad755d0ac2578303b874b92a394958",
+    "url": "/static/media/house.54ad755d.jpg"
+  }
+]);
